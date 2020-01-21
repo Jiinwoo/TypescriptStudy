8f8576d0792b637a30f65ff732b65729650c3f6e
jinwoojung <jwjung5038@gmail.com>
2020-01-21 23:49:23 +0900

    자스스톤 시작

--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -1,32 +1,35 @@
-let numberOne: number = Math.ceil(Math.random() * 9);
-let numberTwo: number = Math.ceil(Math.random() * 9);
+interface Player {
+    hero: HTMLDivElement
+    deck: HTMLDivElement
+    field: HTMLDivElement
+    cost: HTMLDivElement
+    deckData: Card[]
+    heroData: Card | null
+    fieldData: Card[];
+    chosenCard: Card | null;
+    chosenCardData: Card | null;
+}
 
-let result: number = numberOne + numberTwo;
+const opponent: Player = {
+    hero: document.getElementById('rival-hero') as HTMLDivElement,
+    deck: document.getElementById('rival-deck') as HTMLDivElement,
+    field: document.getElementById('rival-field') as HTMLDivElement,
+    cost: document.getElementById('rival-cost') as HTMLDivElement,
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null,
+}
 
-const word1: HTMLDivElement = document.createElement('div');
-word1.textContent = `${numberOne} 곱하기 ${numberTwo} ;`;
-document.body.append(word1);
-
-const form: HTMLFormElement = document.createElement('form');
-const input: HTMLInputElement = document.createElement('input');
-input.type = 'number';
-form.append(input);
-const button: HTMLButtonElement = document.createElement('button');
-button.textContent = '입력!';
-form.append(button);
-document.body.append(form);
-const resultDiv: HTMLDivElement = document.createElement('div');
-document.append(resultDiv);
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (Number(input.value) === result) {
-        resultDiv.textContent = '딩동댕';
-        numberOne = Math.ceil(Math.random() * 9);
-        numberTwo = Math.ceil(Math.random() * 9);
-    } else {
-        resultDiv.textContent = '땡';
-        input.textContent = '';
-        input.focus();
-    }
-});
+const me: Player = {
+    hero: document.getElementById('my-hero'),
+    deck: document.getElementById('me-deck'),
+    field: document.getElementById('me-field'),
+    cost: document.getElementById('me-cost'),
+    deckData: [],
+    heroData: null,
+    fieldData: [],
+    chosenCard: null,
+    chosenCardData: null,
+}
